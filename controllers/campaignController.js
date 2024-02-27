33d1824a79fed921c6d8a7d925ea8d13f635a682
Atharva884 <atharvalolzzz96@gmail.com>
2024-02-27 19:01:56 +0530

    Attatchment done

--- controllers/campaignController.js
+++ controllers/campaignController.js
@@ -4,6 +4,23 @@ dotenv.config();
 const Campaign = require("../models/Campaign");
 
 exports.addCampaign = async function (req, res) {
+  if (req.files) {
+    if (req.files.file) {
+      if (!Array.isArray(req.files)) {
+        let file = req.files.file;
+        const fileName = new Date().getTime().toString() + "-" + file.name;
+        const savePath = path.join(
+          __dirname,
+          "../public/",
+          "attachments",
+          fileName
+        );
+        await file.mv(savePath);
+        req.body.attachment = file;
+      }
+    }
+  }
+
   let campaign = new Campaign(req.body);
   await campaign.createCampaign();
   res.status(201).json({ message: "campaign added" });
